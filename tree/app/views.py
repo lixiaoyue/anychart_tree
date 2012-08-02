f2a881b54b3b89d4af5e072c7f9c2ee7385e7403
olga <olga@irnok.net>
2012-08-02 13:36:05 +0900

    gitignore

--- tree/app/views.py
+++ tree/app/views.py
@@ -1,6 +1,7 @@
 # Create your views here.
 # -*- coding: utf-8 -*-
 from django.shortcuts import render_to_response
+from django.conf import settings
 
 def home_page(request):
     return render_to_response('index.html')
