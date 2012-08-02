a8ce58e07c8960fac4a2f1345521ccdd243ef989
olga <olga@irnok.net>
2012-08-02 17:29:56 +0900

    views and dump a litle bit changed

--- tree/app/views.py
+++ tree/app/views.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render_to_response
 from django.conf import settings
-
+from models import Node
 media = settings.MEDIA_URL
 
 def home_page(request):
+    nodes = Node.objects.all()
     return render_to_response('index.html',{'media':media})
