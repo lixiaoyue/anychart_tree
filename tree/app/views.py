512aeca308879b264236743eb1fa783c45db5866
olga <olga@irnok.net>
2012-08-02 13:42:24 +0900

    .gitignore should work

--- tree/app/views.py
+++ tree/app/views.py
@@ -1,6 +1,7 @@
 # Create your views here.
 # -*- coding: utf-8 -*-
 from django.shortcuts import render_to_response
+from django.template import  RequestContext, Template
 from django.conf import settings
 
 def home_page(request):
