9e43b3f260d641d9cf9c023bc940b6feb3fa3ead
maresha <maresha@list.ru>
2012-08-07 10:40:00 +0900

    добавлен шаблон для отображения дерева

--- tree/app/views.py
+++ tree/app/views.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render_to_response
 from django.conf import settings
-from models import Node
+from django.template.context import RequestContext
+
+from app.models import Node
 media = settings.MEDIA_URL
 
 def home_page(request):
     nodes = Node.objects.all()
     return render_to_response('index.html',{'media':media})
+
+def show_nodes(request):
+    return render_to_response("nodes.html",
+            {'nodes':Node.objects.all()},
+        context_instance=RequestContext(request))
